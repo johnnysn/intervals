a3626fc280946d91176eec0976b1373afa3cf785
Johnny <johnny.gomes@ufjf.br>
2023-09-29 12:02:13 -0300

    feat: Hero section

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,27 +1,41 @@
-import React from 'react'
-import { bebas } from './fonts'
-import Link from 'next/link'
-import Image from 'next/image'
+import React from 'react';
+import { bebas } from './fonts';
+import Link from 'next/link';
+import Image from 'next/image';
 
-type Props = {}
+type Props = {};
 
 export default function Header({}: Props) {
   return (
-    <header className='min-h-[96px] flex items-center justify-center px-2 md:px-6'>
-      <div className='flex justify-between items-center w-full max-w-screen-xl'>
-        <div className={`${bebas.className} text-3xl flex gap-1 items-start`}>
-          <Image src={'/logo.svg'} width={32} height={32} alt='Intervals logo' />
+    <header className="min-h-[96px] flex items-center justify-center px-2 md:px-6">
+      <div className="flex justify-between items-center w-full max-w-screen-xl">
+        <Link
+          href={'/'}
+          className={`${bebas.className} text-3xl flex gap-1 items-start text-gray-50`}
+        >
+          <Image
+            src={'/logo.svg'}
+            width={32}
+            height={32}
+            alt="Intervals logo"
+          />
           <span>Intervals</span>
-        </div>
+        </Link>
 
         <nav>
-          <ul className='flex gap-4 uppercase'>
-            <li><Link href={'/'}>Home</Link></li>
-            <li><Link href={'/treinos'}>Treinos</Link></li>
-            <li><Link href={'/about'}>Sobre</Link></li>
+          <ul className="flex gap-4 uppercase">
+            <li>
+              <Link href={'/treinos'}>Treinos</Link>
+            </li>
+            <li>
+              <Link href={'/about'}>Sobre</Link>
+            </li>
+            <li>
+              <Link href={'/Login'}>Login</Link>
+            </li>
           </ul>
         </nav>
       </div>
     </header>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,28 +4,26 @@ import { bebas } from './fonts';
 export default function Home() {
   return (
     <section id="hero">
-      <div className="flex flex-col md:flex-row-reverse justify-center items-center p-2">
+      <div className="flex flex-col md:flex-row-reverse md:justify-around md:mx-6 md:mt-4 lg:mt-8 justify-center items-center p-2">
         <Image
           src={'/hero.png'}
           alt={'Atleta'}
-          width={316}
-          height={528}
-          className="h-auto w-[150px] md:w-[316px]"
+          width={868}
+          height={389}
+          className="h-auto w-[320px] md:w-[50%] lg:w-[60%]"
         />
-        <div className="md:mr-6 flex flex-col gap-2 items-center">
+        <div className="flex flex-col items-center md:items-start gap-4">
           <h2
-            className={`${bebas.className} text-6xl md:text-8xl leading-none tracking-wide`}
+            className={`${bebas.className} text-6xl md:text-7xl lg:text-8xl leading-none tracking-wide`}
           >
             Potencialize <br />o seu{' '}
-            <span className="font-semibold">treino</span> <br />
+            <span className="font-light">treino</span> <br />
             <span className=" text-teal-300">intervalado</span>
           </h2>
 
-          <p className="font-extralight text-sm max-w-[360px] text-center">
+          <p className="font-extralight text-sm max-w-[360px] text-center md:text-left">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis
-            repellat temporibus dolores pariatur eum error similique. Adipisci
-            ratione deleniti, mollitia consequatur laudantium ullam libero
-            dolor, tempora cupiditate explicabo sint. Autem?
+            repellat temporibus dolores pariatur eum error similique.
           </p>
 
           <button className={`mt-2 border border-teal-300 py-2 px-8 rounded-md bg-transparent hover:bg-teal-300 ${bebas.className} text-teal-300 hover:text-zinc-950 transition-colors duration-300 text-center text-2xl`}>
