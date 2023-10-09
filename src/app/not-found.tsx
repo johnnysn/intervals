a8908a862c94f9b71bb0a0c92fe142bd653895eb
Johnny <johnny.gomes@ufjf.br>
2023-10-09 09:45:09 -0300

    feat: Add not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function NotFound() {
+  return (
+    <div>
+      <h2 className='text-2xl leading-loose'>Página não encontrada</h2>
+
+      <p>
+        A página requisitada não existe ou está em construção.
+      </p>
+    </div>
+  )
+}
